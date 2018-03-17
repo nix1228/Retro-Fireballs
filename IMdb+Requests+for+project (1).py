e93cab23fa60b591879bcb3ceb90f96f52810137
Nikita <nix1228@gmail.com>
2018-03-17 11:44:31 -0700

    imdb data

--- IMdb+Requests+for+project (1).py
+++ IMdb+Requests+for+project (1).py
@@ -41,7 +41,7 @@ def view_imdb_data_ratings(season_number,episode_number):
 # In[4]:
 
 
-#season_one
+#season_one votes
 season_one=[]
 for y in range(11):
         try:
@@ -63,12 +63,6 @@ for i in range(len(season_one)):
 # In[6]:
 
 
-season_one
-
-
-# In[7]:
-
-
 #season_two
 season_two=[]
 for y in range(11):
@@ -79,7 +73,7 @@ for y in range(11):
     
 
 
-# In[8]:
+# In[7]:
 
 
 season_two
@@ -87,13 +81,7 @@ for i in range(len(season_two)):
     season_two[i]=int(season_two[i])
 
 
-# In[9]:
-
-
-season_two
-
-
-# In[10]:
+# In[8]:
 
 
 #season_three
@@ -105,7 +93,7 @@ for y in range(11):
         continue
 
 
-# In[11]:
+# In[9]:
 
 
 season_three
@@ -113,13 +101,7 @@ for i in range(len(season_three)):
     season_three[i]=int(season_three[i])
 
 
-# In[12]:
-
-
-season_three
-
-
-# In[13]:
+# In[10]:
 
 
 #season_four
@@ -131,7 +113,7 @@ for y in range(11):
         continue
 
 
-# In[14]:
+# In[11]:
 
 
 season_four
@@ -139,13 +121,7 @@ for i in range(len(season_four)):
     season_four[i]=int(season_four[i])
 
 
-# In[15]:
-
-
-season_four
-
-
-# In[16]:
+# In[12]:
 
 
 #season_five
@@ -157,7 +133,7 @@ for y in range(11):
         continue
 
 
-# In[17]:
+# In[13]:
 
 
 season_five
@@ -165,13 +141,7 @@ for i in range(len(season_five)):
     season_five[i]=int(season_five[i])
 
 
-# In[18]:
-
-
-season_five
-
-
-# In[19]:
+# In[14]:
 
 
 #season 6
@@ -183,7 +153,7 @@ for y in range(11):
         continue
 
 
-# In[20]:
+# In[15]:
 
 
 season_six
@@ -191,13 +161,7 @@ for i in range(len(season_six)):
     season_six[i]=int(season_six[i])
 
 
-# In[21]:
-
-
-season_six
-
-
-# In[22]:
+# In[16]:
 
 
 #season 7
@@ -209,29 +173,14 @@ for y in range(8):
             continue
 
 
-# In[23]:
-
-
-season_seven.append('0')
-season_seven.append('0')
-season_seven.append('0')
-season_seven
-
-
-# In[24]:
+# In[17]:
 
 
 for i in range(len(season_seven)):
     season_seven[i]=int(season_seven[i])
 
 
-# In[25]:
-
-
-season_seven
-
-
-# In[26]:
+# In[18]:
 
 
 votes_df=pd.DataFrame({"Season 1":season_one,
@@ -240,18 +189,19 @@ votes_df=pd.DataFrame({"Season 1":season_one,
                           "Season 4":season_four,
                           "Season 5":season_five,
                           "Season 6":season_six,
-                          "Season 7":season_seven})
+                          })
 
 
-# In[27]:
+# In[19]:
 
 
-votes_df
+votes_df_season_seven=pd.DataFrame({"Season 7":season_seven})
 
 
-# In[28]:
+# In[20]:
 
 
+#now for the ratings
 season_one_ratings=[]
 for y in range(11):
         try:
@@ -260,7 +210,7 @@ for y in range(11):
             continue
 
 
-# In[29]:
+# In[21]:
 
 
 season_one_ratings
@@ -268,13 +218,7 @@ for i in range(len(season_one_ratings)):
     season_one_ratings[i]=float(season_one_ratings[i])
 
 
-# In[30]:
-
-
-season_one_ratings
-
-
-# In[31]:
+# In[22]:
 
 
 season_two_ratings=[]
@@ -285,7 +229,7 @@ for y in range(11):
         continue
 
 
-# In[32]:
+# In[23]:
 
 
 season_two_ratings
@@ -293,13 +237,7 @@ for i in range(len(season_two_ratings)):
     season_two_ratings[i]=float(season_two_ratings[i])
 
 
-# In[33]:
-
-
-season_two_ratings
-
-
-# In[34]:
+# In[24]:
 
 
 season_three_ratings=[]
@@ -310,7 +248,7 @@ for y in range(11):
         continue
 
 
-# In[35]:
+# In[25]:
 
 
 season_three_ratings
@@ -318,13 +256,7 @@ for i in range(len(season_three_ratings)):
     season_three_ratings[i]=float(season_three_ratings[i])
 
 
-# In[36]:
-
-
-season_three_ratings
-
-
-# In[37]:
+# In[26]:
 
 
 season_four_ratings=[]
@@ -335,7 +267,7 @@ for y in range(11):
         continue
 
 
-# In[38]:
+# In[27]:
 
 
 season_four_ratings
@@ -343,24 +275,18 @@ for i in range(len(season_four_ratings)):
     season_four_ratings[i]=float(season_four_ratings[i])
 
 
-# In[39]:
-
-
-season_four_ratings
-
-
-# In[40]:
+# In[28]:
 
 
 season_five_ratings=[]
 for y in range(11):
     try:
-        season_five_ratings.append(view_imdb_data_ratings(4,y))
+        season_five_ratings.append(view_imdb_data_ratings(5,y))
     except:
         continue
 
 
-# In[41]:
+# In[29]:
 
 
 season_five_ratings
@@ -368,24 +294,18 @@ for i in range(len(season_five_ratings)):
     season_five_ratings[i]=float(season_five_ratings[i])
 
 
-# In[42]:
-
-
-season_five_ratings
-
-
-# In[43]:
+# In[30]:
 
 
 season_six_ratings=[]
 for y in range(11):
     try:
-        season_six_ratings.append(view_imdb_data_ratings(4,y))
+        season_six_ratings.append(view_imdb_data_ratings(6,y))
     except:
         continue
 
 
-# In[44]:
+# In[31]:
 
 
 season_six_ratings
@@ -393,13 +313,7 @@ for i in range(len(season_six_ratings)):
     season_six_ratings[i]=float(season_six_ratings[i])
 
 
-# In[45]:
-
-
-season_six_ratings
-
-
-# In[46]:
+# In[32]:
 
 
 season_seven_ratings=[]
@@ -411,28 +325,14 @@ for y in range(8):
     
 
 
-# In[47]:
-
-
-season_seven_ratings.append(0)
-season_seven_ratings.append(0)
-season_seven_ratings.append(0)
-
-
-# In[48]:
+# In[33]:
 
 
 for i in range(len(season_seven_ratings)):
     season_seven_ratings[i]=float(season_seven_ratings[i])
 
 
-# In[49]:
-
-
-season_seven_ratings
-
-
-# In[50]:
+# In[34]:
 
 
 ratings_df=pd.DataFrame({"Season 1":season_one_ratings,
@@ -441,60 +341,47 @@ ratings_df=pd.DataFrame({"Season 1":season_one_ratings,
                           "Season 4":season_four_ratings,
                           "Season 5":season_five_ratings,
                           "Season 6":season_six_ratings,
-                           "Season 7":season_seven_ratings})
+                           })
 
 
-# In[51]:
+# In[35]:
 
 
-ratings_df
+ratings_season_seven_df=pd.DataFrame({"Season 7":season_seven_ratings})
 
 
-# In[52]:
-
-
-ratings_df["Season 1"]
-
-
-# In[53]:
+# In[36]:
 
 
 x_axis=["Ep.1","Ep.2","Ep.3","Ep.4","Ep.5","Ep.6","Ep.7","Ep.8","Ep.9","Ep.10"]
 y_axis=ratings_df["Season 1"]
-
-
-# In[62]:
-
-
-plt.plot(x_axis,y_axis, label="Season 1",color="gold")
+plt.plot(x_axis,y_axis, label="Season 1",color="black")
 y_axis=ratings_df["Season 2"]
-plt.plot(x_axis,y_axis, label="Season 2",color="lightcoral")
+plt.plot(x_axis,y_axis, label="Season 2", color="lightcoral")
 y_axis=ratings_df["Season 3"]
-plt.plot(x_axis,y_axis, label="Season 3",color="lightskyblue")
+plt.plot(x_axis,y_axis, label="Season 3", color="lightskyblue")
 y_axis=ratings_df["Season 4"]
-plt.plot(x_axis,y_axis, label="Season 4",color="red")
+plt.plot(x_axis,y_axis, label="Season 4", color="red")
 y_axis=ratings_df["Season 5"]
-plt.plot(x_axis,y_axis, label="Season 5",color="purple")
+plt.plot(x_axis,y_axis, label="Season 5", color="darkblue")
 y_axis=ratings_df["Season 6"]
-plt.plot(x_axis,y_axis, label="Season 6",color="green")
-y_axis=ratings_df["Season 7"]
-plt.plot(x_axis,y_axis, label="Season 7",color="orange")
-plt.ylim(7,10.0)
-plt.legend()
+plt.plot(x_axis,y_axis, label="Season 6", color="green")
+x_axis=["Ep.1","Ep.2","Ep.3","Ep.4","Ep.5","Ep.6","Ep.7"]
+y_axis=ratings_season_seven_df
+plt.plot(x_axis,y_axis, label="Season 7", color="orange")
+plt.legend(loc="best")
+plt.title("Ratings Per Episode in Each Season")
+plt.xlabel("Episode")
+plt.ylabel("Rating Score")
 plt.show()
 
 
-# In[55]:
+# In[37]:
 
 
 x_axis=["Ep.1","Ep.2","Ep.3","Ep.4","Ep.5","Ep.6","Ep.7","Ep.8","Ep.9","Ep.10"]
 y_axis=votes_df["Season 1"]
-
-
-# In[56]:
-
-
-plt.plot(x_axis,y_axis, label="Season 1",color="gold")
+plt.plot(x_axis,y_axis, label="Season 1",color="black")
 y_axis=votes_df["Season 2"]
 plt.plot(x_axis,y_axis, label="Season 2",color="lightcoral")
 y_axis=votes_df["Season 3"]
@@ -505,10 +392,100 @@ y_axis=votes_df["Season 5"]
 plt.plot(x_axis,y_axis, label="Season 5",color="purple")
 y_axis=votes_df["Season 6"]
 plt.plot(x_axis,y_axis, label="Season 6",color="green")
-y_axis=votes_df["Season 7"]
-plt.plot(x_axis,y_axis, label="Season 7",color="orange")
+x_axis=["Ep.1","Ep.2","Ep.3","Ep.4","Ep.5","Ep.6","Ep.7"]
+y_axis=votes_df_season_seven
+plt.plot(x_axis,y_axis, label="Season 7",color="darkblue")
+plt.title("IMdB votes per Episode by Season")
+plt.xlabel("Episode Number")
+plt.ylabel("Number of Votes Per Episode ")
+plt.plot(x_axis,y_axis)
 plt.legend()
 plt.show()
-plt.plot(x_axis,y_axis)
+
+
+# In[57]:
+
+
+avg_rating_season_one=ratings_df["Season 1"].mean()
+avg_rating_season_two=ratings_df["Season 2"].mean()
+avg_rating_season_three=ratings_df["Season 3"].mean()
+avg_rating_season_four=ratings_df["Season 4"].mean()
+avg_rating_season_five=ratings_df["Season 5"].mean()
+avg_rating_season_six=ratings_df["Season 6"].mean()
+avg_rating_season_seven=ratings_season_seven_df.mean()
+
+
+# In[58]:
+
+
+x_axis=["Season 1"]
+y_axis=avg_rating_season_one
+plt.bar(x_axis,y_axis)
+x_axis=["Season 2"]
+y_axis=avg_rating_season_two
+plt.bar(x_axis,y_axis)
+x_axis=["Season 3"]
+y_axis=avg_rating_season_three
+plt.bar(x_axis,y_axis)
+x_axis=["Season 4"]
+y_axis=avg_rating_season_four
+plt.bar(x_axis,y_axis)
+x_axis=["Season 5"]
+y_axis=avg_rating_season_five
+plt.bar(x_axis,y_axis)
+x_axis=["Season 6"]
+y_axis=avg_rating_season_six
+plt.bar(x_axis,y_axis)
+x_axis=["Season 7"]
+y_axis=avg_rating_season_seven
+plt.bar(x_axis,y_axis)
+plt.ylim(6.0,10.0)
+plt.title("Average Ratings Per Season")
+plt.xlabel("Season")
+plt.ylabel("Average Rating")
+plt.xticks(rotation=45)
+plt.show()
+
+
+# In[55]:
+
+
+avg_votes_season_one=votes_df["Season 1"].mean()
+avg_votes_season_two=votes_df["Season 2"].mean()
+avg_votes_season_three=votes_df["Season 3"].mean()
+avg_votes_season_four=votes_df["Season 4"].mean()
+avg_votes_season_five=votes_df["Season 5"].mean()
+avg_votes_season_six=votes_df["Season 6"].mean()
+avg_votes_season_seven=votes_df_season_seven.mean()
+
+
+# In[56]:
+
+
+x_axis="Season 1"
+y_axis=avg_votes_season_one
+plt.bar(x_axis,y_axis)
+x_axis="Season 2"
+y_axis=avg_votes_season_two
+plt.bar(x_axis,y_axis)
+x_axis="Season 3"
+y_axis=avg_votes_season_three
+plt.bar(x_axis,y_axis)
+x_axis="Season 4"
+y_axis=avg_votes_season_four
+plt.bar(x_axis,y_axis)
+x_axis="Season 5"
+y_axis=avg_votes_season_five
+plt.bar(x_axis,y_axis)
+x_axis="Season 6"
+y_axis=avg_votes_season_six
+plt.bar(x_axis,y_axis)
+x_axis="Season 7"
+y_axis=avg_votes_season_seven
+plt.bar(x_axis,y_axis)
+plt.xticks(rotation=45)
+plt.title("Average Votes per Season")
+plt.xlabel("Season")
+plt.ylabel("Average Number of Votes")
 plt.show()
 
